0305d8bcf21e638cf05f0e19c89bc3a22d09128c
Lucas Duncan <lucasdonuts@gmail.com>
2022-07-05 17:48:44 -0500

    Search results are now rendered on the page. Some styling

--- js/index.js
+++ js/index.js
@@ -2,19 +2,46 @@
 
 document.addEventListener('DOMContentLoaded', () => {
   const searchForm = document.querySelector('#github-form');
-  let searchResults;
+  const userList = document.querySelector('#user-list')
 
   searchForm.addEventListener('submit', (e) => {
     e.preventDefault();
 
     fetchSearchResults(e.target.search.value)
-    .then(data => searchResults = data.items)
-    .then(() => console.log(searchResults))
+    .then(data => renderSearchResults(data.items))
+
+    searchForm.reset();
   })
 
+  // Search function
   function fetchSearchResults(name) {
     return fetch(`https://api.github.com/search/users?q=${name}`)
     .then(res => res.json())
+  }
+
+  // Displaying search results
+  function renderSearchResults(results) {
+    results.forEach((result) => {
+      createUserCard(result);
+    })
+  }
+
+  function createUserCard(user) {
+    const li = document.createElement('li')
+    const h2 = document.createElement('h2')
+    const img = document.createElement('img')
+    const link = document.createElement('a')
+
+    li.className = 'user-card'
+    li.id = user.login
+    h2.innerText = user.login
+    img.src = user.avatar_url
+    img.className = 'user-img'
+    link.href = `https://www.github.com/${user.login}`
+
+    userList.append(li)
+    li.append(link)
+    link.append(img, h2)
   }
 
 })
